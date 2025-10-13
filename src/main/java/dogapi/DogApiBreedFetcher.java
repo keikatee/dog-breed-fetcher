232e053e6472fdfaec002767577911187b8a6205
keikatee <keito.hara8@gmail.com>
2025-10-13 16:02:36 -0400

    Implement DogApiBreedFetcher

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -25,11 +25,33 @@ public class DogApiBreedFetcher implements BreedFetcher {
      */
     @Override
     public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
-        //      and the documentation for the dog.ceo API. You may find it helpful
-        //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+        String url = "https://dog.ceo/api/breed/" + breed + "/list"; // Constructing the API URL
+        Request request = new Request.Builder().url(url).build(); //Building the HTTP request
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            if (!response.isSuccessful()) {
+                throw new BreedNotFoundException("Failed to call API for breed: " + breed);
+            }
+
+
+            JSONObject responseBody = new JSONObject(response.body().string());//Reading the JSON body
+
+            String status = responseBody.getString("status");
+            if (status.equals("error")) {
+                throw new BreedNotFoundException("Breed not found: " + breed);
+            }// check the response if succeed
+
+            JSONArray jsonArray = responseBody.getJSONArray("message");//Extracting the “message” array
+            List<String> subBreeds = new ArrayList<>();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                subBreeds.add(jsonArray.getString(i));
+            }
+            return subBreeds;
+
+        } catch (IOException e) {//Handling errors
+            throw new BreedNotFoundException("Error fetching sub-breeds: " + e.getMessage());
+        }
     }
 }
